d73ebc7f952a4a7328eae2996b53475e61c1bd63
priaajisn <priaaji.setyanegoro@gmail.com>
2020-02-12 13:06:10 +0700

    add PORT

--- server.js
+++ server.js
@@ -76,8 +76,8 @@ app.post("/imageUrl", (req, res) => {
   image.handleImageUrlApi(req, res);
 });
 
-app.listen(3000, () => {
-  console.log("app is running on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
 });
 
 /*
